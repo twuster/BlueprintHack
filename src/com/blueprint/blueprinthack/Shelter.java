54bb75cc3791ed710d4c47254f12667e7b2474b6
Gavin Chu <gavincca@gmail.com>
2014-03-08 18:55:16 -0800

    gridview setup

---
+++ src/com/blueprint/blueprinthack/Shelter.java
@@ -0,0 +1,19 @@
+package com.blueprint.blueprinthack;
+
+public class Shelter {
+	
+	private String name;
+	private String address;
+	private String hours;
+	private String email;
+	private String phone;
+	
+	public Shelter (String name, String address, String hours, String email, String phone){
+		this.name = name;
+		this.address = address;
+		this.hours = hours;
+		this.email = email;
+		this.phone = phone;
+	}
+
+}
