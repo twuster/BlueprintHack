00597e07a472258b4615f7d72b97bc585b468c01
Tony Wu <tony.wu@berkeley.edu>
2014-03-08 21:24:59 -0800

    fixed conflict in MainActivity

--- src/com/blueprint/blueprinthack/MainActivity.java
+++ src/com/blueprint/blueprinthack/MainActivity.java
@@ -41,17 +41,13 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-<<<<<<< HEAD
-    
+
     private int[] pet1photos = { R.drawable.sample_2, R.drawable.sample_3, R.drawable.sample_4 };
     Shelter shelter1 = new Shelter("Shelter 1", "123 Pet Avenue", "Mon-Fri 9am-5pm", "pet@gmail.com", "123-456-7890");
     Pet dog1 = new Pet("dog 1", shelter1, 2, pet1photos, R.drawable.sample_1, "android.resource://" + getPackageName() + "/" 
     		+ R.raw.dog_video);
     private Pet[] allPets = {dog1, dog1, dog1, dog1};
     private Pet[] dogs = {};
-    
-    
-=======
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
@@ -72,5 +68,4 @@ public class MainActivity extends Activity {
         }
         return true;
     }
->>>>>>> 58aff39fc668f187dbca64ccd61dc7bbf8891994
 }
