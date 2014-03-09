7cf051dea4382bb0b14053abf397fcafb8fdfd15
Tony Wu <tony.wu@berkeley.edu>
2014-03-09 00:11:43 -0800

    Added ShelterActivity and edited app name.

--- src/com/blueprint/blueprinthack/Shelter.java
+++ src/com/blueprint/blueprinthack/Shelter.java
@@ -1,12 +1,9 @@
 package com.blueprint.blueprinthack;
 
-import android.app.Activity;
-import android.os.Bundle;
 import android.os.Parcel;
 import android.os.Parcelable;
-import android.widget.TextView;
 
-public class Shelter extends Activity implements Parcelable {
+public class Shelter implements Parcelable {
 	
 	private String name;
 	private String address;
@@ -14,8 +11,7 @@ public class Shelter extends Activity implements Parcelable {
 	private String email;
 	private String phone;
 	
-	private TextView name_text, address2, hours2, contact_email, contact_phone;
-	
+
 	public Shelter (String name, String address, String hours, String email, String phone){
 		this.name = name;
 		this.address = address;
@@ -23,23 +19,7 @@ public class Shelter extends Activity implements Parcelable {
 		this.email = email;
 		this.phone = phone;
 	}
-	
-	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.shelter_info);
-        
-        name_text = (TextView) findViewById(R.id.name);
-        name_text.setText(this.name);
-        address2 = (TextView) findViewById(R.id.address2);
-        address2.setText(this.address);
-        hours2 = (TextView) findViewById(R.id.hours2);
-        hours2.setText(this.hours);
-        contact_email = (TextView) findViewById(R.id.contact2);
-        contact_email.setText(this.email);
-        contact_phone = (TextView) findViewById(R.id.contact3);
-        contact_phone.setText(this.phone);
-    }
+
 	
 	public String getName(){
 		return this.name;
---
+++ src/com/blueprint/blueprinthack/ShelterActivity.java
@@ -0,0 +1,56 @@
+package com.blueprint.blueprinthack;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by tony on 3/8/14.
+ */
+public class ShelterActivity  extends Activity {
+
+    private TextView name_text, address, addressField, hours, hoursField, contact, contact_email, contact_phone;
+    private Button getDirections, fbShare;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.shelter_info);
+
+        Bundle b = getIntent().getExtras();
+        Shelter s = b.getParcelable("shelter");
+
+
+        name_text = (TextView) findViewById(R.id.name);
+        name_text.setText(s.getName());
+        address = (TextView) findViewById(R.id.address);
+        addressField = (TextView) findViewById(R.id.address_field);
+        addressField.setText(s.getAddress());
+        hours = (TextView) findViewById(R.id.hours);
+        hoursField = (TextView) findViewById(R.id.hours_field);
+        hoursField.setText(s.getHours());
+        contact = (TextView) findViewById(R.id.contact);
+        contact_email = (TextView) findViewById(R.id.contact_email);
+        contact_email.setText(s.getEmail());
+        contact_phone = (TextView) findViewById(R.id.contact_phone);
+        contact_phone.setText(s.getPhone());
+
+        getDirections = (Button) findViewById(R.id.directions_button);
+        getDirections.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        fbShare = (Button) findViewById(R.id.fb_button);
+        fbShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
